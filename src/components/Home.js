f4a2f1cb181139e7e090366caf142f3eee90b0a9
tsaristbomba <andrexerdna@gmail.com>
2021-01-12 08:56:01 -0300

    resolving bugs

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,7 +57,7 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    latitude !== null && getMyCity();
+    latitude !== null && longitude !== null && getMyCity();
   }, [latitude, longitude]);
 
   useEffect(() => {
